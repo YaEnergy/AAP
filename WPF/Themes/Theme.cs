107fe67768ddbbf215de65e13018fe42085c4401
YaEnergy <kiano.van.cauter@gmail.com>
2023-08-16 10:44:29 +0200

    UI Improvements

--- WPF/Themes/Theme.cs
+++ WPF/Themes/Theme.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,44 +10,32 @@ namespace AAP.UI
 {
     public class Theme
     {
-        public readonly System.Windows.Media.Brush Background = System.Windows.Media.Brushes.White;
-        public readonly System.Windows.Media.Brush Art = System.Windows.Media.Brushes.Black;
-        public readonly System.Windows.Media.Brush Border = System.Windows.Media.Brushes.Black;
+        public Brush Background { get; set; } = Brushes.White;
+        public Brush Accent { get; set; } = new SolidColorBrush(Color.FromRgb(242, 242, 242));
 
-        public readonly System.Windows.Media.Brush Primary = System.Windows.Media.Brushes.White;
-        public readonly System.Windows.Media.Brush Secondary = System.Windows.Media.Brushes.Gray;
+        public Brush Art { get; set; } = Brushes.Black;
+        public Brush Border { get; set; } = Brushes.Black;
 
-        public readonly System.Windows.Media.Brush PrimaryActivated = System.Windows.Media.Brushes.Green;
-        public readonly System.Windows.Media.Brush SecondaryActivated = System.Windows.Media.Brushes.DarkGreen;
+        public Brush Primary { get; set; } = Brushes.White;
+        public Brush Secondary { get; set; } = Brushes.Gray;
+
+        public Brush PrimaryActive { get; set; } = Brushes.Green;
+        public Brush SecondaryActive { get; set; } = Brushes.DarkGreen;
 
         public Theme()
         {
-            Background.Freeze();
-            Art.Freeze();
-            Border.Freeze();
-            Primary.Freeze();
-            Secondary.Freeze();
-            PrimaryActivated.Freeze();
-            SecondaryActivated.Freeze();
+
         }
 
-        public Theme(System.Windows.Media.Brush background, System.Windows.Media.Brush art, System.Windows.Media.Brush border, System.Windows.Media.Brush primary, System.Windows.Media.Brush secondary, System.Windows.Media.Brush primaryActivated, System.Windows.Media.Brush secondaryActivated)
+        public Theme(Brush background, Brush art, Brush border, Brush primary, Brush secondary, Brush primaryActive, Brush secondaryActive)
         {
             Background = background;
             Art = art;
             Border = border;
             Primary = primary; 
             Secondary = secondary;
-            PrimaryActivated = primaryActivated;
-            SecondaryActivated = secondaryActivated;
-
-            Background.Freeze();
-            Art.Freeze();
-            Border.Freeze();
-            Primary.Freeze();
-            Secondary.Freeze();
-            PrimaryActivated.Freeze();
-            SecondaryActivated.Freeze();
+            PrimaryActive = primaryActive;
+            SecondaryActive = secondaryActive;
         }
     }
 }
---
+++ WPF/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using AAP.Timelines;
+using AAP.UI.Controls;
+
+namespace AAP.UI.ViewModels
+{
+    public class MainWindowViewModel : INotifyPropertyChanged
+    {
+        private bool isToolboxVisible = true;
+        public bool IsToolboxVisible
+        {
+            get => isToolboxVisible;
+            set
+            {
+                if (isToolboxVisible == value)
+                    return;
+
+                isToolboxVisible = value;
+
+                ToolboxVisibility = value ? Visibility.Visible : Visibility.Collapsed;
+
+                PropertyChanged?.Invoke(this, new(nameof(IsToolboxVisible)));
+            }
+        }
+
+        private Visibility toolboxVisibility = Visibility.Visible;
+        public Visibility ToolboxVisibility
+        {
+            get => toolboxVisibility;
+            private set
+            {
+                if (toolboxVisibility == value)
+                    return;
+
+                toolboxVisibility = value;
+                PropertyChanged?.Invoke(this, new(nameof(ToolboxVisibility)));
+            }
+        }
+
+        private bool isLayerManagementVisible = true;
+        public bool IsLayerManagementVisible
+        {
+            get => isLayerManagementVisible;
+            set
+            {
+                if (isLayerManagementVisible == value)
+                    return;
+
+                isLayerManagementVisible = value;
+
+                LayerManagementVisibility = value ? Visibility.Visible : Visibility.Collapsed;
+
+                PropertyChanged?.Invoke(this, new(nameof(IsLayerManagementVisible)));
+            }
+        }
+
+        private Visibility layerManagementVisibility = Visibility.Visible;
+        public Visibility LayerManagementVisibility
+        {
+            get => layerManagementVisibility;
+            private set
+            {
+                if (layerManagementVisibility == value)
+                    return;
+
+                layerManagementVisibility = value;
+                PropertyChanged?.Invoke(this, new(nameof(LayerManagementVisibility)));
+            }
+        }
+
+        private Theme windowTheme = new();
+        public Theme WindowTheme
+        {
+            get => windowTheme;
+            set
+            {
+                if (windowTheme == value)
+                    return;
+
+                windowTheme = value;
+                PropertyChanged?.Invoke(this, new(nameof(WindowTheme)));
+            }
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public MainWindowViewModel()
+        {
+
+        }
+    }
+}
